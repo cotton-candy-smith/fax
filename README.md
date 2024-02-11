47fdee463fe57d29ff6dabb1fd45c76efe510761
João Pedro <joaopjt@icloud.com>
2024-02-11 12:32:36 -0300

    v0.0.3 - release

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A wrapper for chinese characters set on utf-8 charset
 
 CHANGELOG
 -----------------------------
+v0.0.3: Added greenland key's a array with 2 (two) childrens in it. Added enum.js and index.js files on src/data folder.
+
 v0.0.2: Added src/data folder with beijing, hong-kong, nanjing, shangai, wuhan and xi-an folders.
 
 v0.0.1: Added project folder.
---
+++ src/data/index.js
@@ -0,0 +1,52 @@
+import { CONTRIES, COUNTRY_KEYS } from 'enum';
+
+const shortcuts = [{
+	beijing: {
+		beijing: CONTRIES['AMERICA'],
+		hong_kong: CONTRIES['GERMANY'],
+		nanjing: CONTRIES['UK'],
+		shanghai: CONTRIES['UKRAINE'],
+		wuhan: CONTRIES['BRAZIL'],
+		xi_an: CONTRIES['SWITZERLAND'],
+	},
+	hong_kong: {
+		beijing: CONTRIES['CANADA'],
+		hong_kong: CONTRIES['PERU'],
+		nanjing: CONTRIES['JAPAN'],
+		shanghai: CONTRIES['AFGHANISTAN'],
+		wuhan: CONTRIES['PERU'],
+		xi_an: CONTRIES['MONGOLIA'],
+	},
+	nanjing: {
+		beijing: CONTRIES['RUSSIA'],
+		hong_kong: CONTRIES['KYRGYZSTAN'],
+		nanjing: CONTRIES['ITALY'],
+		shanghai: CONTRIES['VIETNAM'],
+		wuhan: CONTRIES['QATAR'],
+		xi_an: CONTRIES['UKRAINE'],
+	},
+	shanghai: {
+		beijing: CONTRIES['BRAZIL'],
+		hong_kong: CONTRIES['IRELAND'],
+		nanjing: CONTRIES['VENEZUELA'],
+		shanghai: CONTRIES['INDIA'],
+		wuhan: CONTRIES['CHINA'],
+		xi_an: CONTRIES['FRANCE'],
+	},
+	wuhan: {
+		beijing: CONTRIES['AMERICA'],
+		hong_kong: CONTRIES['KAZAKHSTAN'],
+		nanjing: CONTRIES['ROMANIA'],
+		shanghai: CONTRIES['SOUTH_AFRICA'],
+		wuhan: CONTRIES['AUSTRIA'],
+		xi_an: CONTRIES['FRANCE'],
+	},
+	xi_an: {
+		beijing: CONTRIES['GUATEMALA'],
+		hong_kong: CONTRIES['MYANMAR'],
+		nanjing: CONTRIES['PORTUGAL'],
+		shanghai: CONTRIES['MEXICO'],
+		wuhan: CONTRIES['PAPUA_NEW_GUINEA'],
+		xi_an: CONTRIES['NEW_ZEALAND'],
+	}
+}];
